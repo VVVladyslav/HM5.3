fe948e793ac310173cf27fb354e5bf41cc100ae2
VVVladyslav <vladoszhar@gmail.com>
2023-12-09 22:10:41 +0200

    w

--- src/Dinamic.java
+++ src/Dinamic.java
@@ -1,18 +1,18 @@
 public class Dinamic {
 
-    public static int findNumber(int findNumber) {
-        if (findNumber <= 1) {
-            return findNumber;
+    public static int findNumber(int nNumberToSearch) {
+        if (nNumberToSearch <= 1) {
+            return nNumberToSearch;
         }
 
-        int[] result = new int[findNumber + 1];
+        int[] result    = new int[nNumberToSearch + 1];
         result[0] = 0;
         result[1] = 1;
 
-        for (int i = 2; i <= findNumber; i++) {
+        for (int i = 2; i <= nNumberToSearch; i++) {
             result[i] = result[i - 1] + result[i - 2];
         }
 
-        return result[findNumber];
+        return result[nNumberToSearch];
     }
 }
